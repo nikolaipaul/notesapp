801902735d05b8dec2ac8524e34f6e0d0906786c
asd <asd>
2022-01-23 16:59:23 +0100

    fix localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:quicknote/screens/groups_screen.dart';
 import 'package:quicknote/screens/notes_screen.dart';
 
@@ -10,13 +11,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Quicknote';
-
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
-      title: _title,
+      title: 'App',
       localizationsDelegates: [
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
@@ -40,11 +40,13 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(items: const <BottomNavigationBarItem>[
+        tabBar: CupertinoTabBar(items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.folder), label: 'Groups'),
+              icon: const Icon(CupertinoIcons.folder),
+              label: AppLocalizations.of(context)?.groups ?? 'Groups'),
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.doc_text), label: 'Notes')
+              icon: const Icon(CupertinoIcons.doc_text),
+              label: AppLocalizations.of(context)?.notes ?? 'Notes')
         ]),
         tabBuilder: (context, index) => CupertinoTabView(
             builder: (context) => index == 0
